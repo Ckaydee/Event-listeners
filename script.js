c9e4b549b96566c5aa02bf95155870785a1ccdb7
Ckaydee <145369629+Ckaydee@users.noreply.github.com>
2023-11-22 09:13:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// EVENT LISTENERS
+
+// Event Listener
+document.getElementById("idiom1").addEventListener("click", idi1);
+document.getElementById("idiom2").addEventListener("click", idi2);
+document.getElementById("idiom3").addEventListener("click", idi3);
+document.getElementById("idiom4").addEventListener("click", idi4);
+document.getElementById("idiom5").addEventListener("click", idi5);
+
+// Event functions
+function idi1() {
+    alert("Thin!");
+}
+
+function idi2(){
+    alert("Bush!")
+}
+
+function idi3() {
+    alert("Bandwagon!");
+}
+
+function idi4() {
+    alert("Rain!");
+}
+
+function idi5() {
+    alert("Your court!");
+}
